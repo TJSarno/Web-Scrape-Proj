987f70cfcec8e985906a3c28eabb457908252666
TJSarno <Thomassarno@hotmail.com>
2023-06-08 19:08:42 +0100

    added an array to store dates, also created a cleaned datestring to later be used as regex

--- scrape_main.py
+++ scrape_main.py
@@ -1,18 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from datetime import datetime
 
 url = "https://www.gamespot.com/articles/2023-upcoming-games-release-schedule/1100-6508202/"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
-#tags = doc.select('p:-soup-contains(" - ")')
+##Working
+dates = []
 tags = doc.find_all('p')
 for tag in tags:
-    print(tag.text)
+    content = tag.text
+    dates.append(content)
 
+#Get current date
+my_date = datetime.today()
+#Convert to 'readable' (e.g. 08 June)
+readable_date = datetime.today().strftime('%d %B')
+#Clean string in case of zero padded number (e.g. 08)
+cleaned_date = readable_date.lstrip('0')
 
+for date in dates:
+    if re.search(str(cleaned_date),date):
+        print("match found")
+        
 #current_release_date = tags
 #prices = doc.find_all(string="$")
 #print(prices)
+# 
